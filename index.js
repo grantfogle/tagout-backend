03e756a87aead962cfc633cf1d68eba01ec09d06
grantfogle <grantjfogle@gmail.com>
2022-04-28 19:07:07 -0600

    added basic convert pdf to text form

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 const express = require("express");
-const app = express();
-const PORT = 4200
+const fileUpload = require('express-fileupload');
+const pdfParse = require('pdf-parse');
 
-app.get('/', (req, res) => {
-    res.send('hey grant')
+const app = express();
+const fs = require('fs');
+
+// app.get('/', (req, res) => {
+//     res.send('hey grant');
+//     getHuntCodePdf();
+// });
+
+app.use('/', express.static('public'));
+app.use(fileUpload());
+
+app.post('/extract-text', (req, res) => {
+    if (!req.files &&  !req.files.pdfFile) {
+        req.status(400)
+        res.end();
+    }
+
+    pdfParse(req.files.pdfFile).then(result => {
+        res.send(result.text);
+    });
 });
 
-app.listen(PORT, () => console.log(`it's alive on http://localhost:${PORT}`));
+// crawl pdf
+// figure out code of unit
+// get res and non res info
+// covert to json
+// upload json to google fireba
+
+
+app.listen(3000)
