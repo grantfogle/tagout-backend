a12302cbb50f643fbbbc0ba72bdd13b278f1c2cc
grantfogle <grantjfogle@gmail.com>
2022-05-09 18:10:35 -0600

    mocking up remove unneeded text string

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const pdfParse = require('pdf-parse');
 
 const app = express();
 const fs = require('fs');
+const { getEnabledCategories } = require("trace_events");
 
 // app.get('/', (req, res) => {
 //     res.send('hey grant');
@@ -24,6 +25,59 @@ app.post('/extract-text', (req, res) => {
     });
 });
 
+app.get('/convert-to-json', async () => {
+    console.log('cats');
+    // await fs.readFile('./elk.txt', function(text) {
+    //     const textByLine = text.split('\n');
+    //     console.log(textByLine);
+    // })
+    var text = fs.readFileSync("./elk.txt", 'utf-8');
+
+    var newlines = text.split('\n');
+    var replacedTexts = removeUnneededText(newlines.join(''));
+
+    console.log(replacedTexts);
+    // remove unneeded text
+    // break up text by preference point
+    // ee0001e1r
+    // then get the preference points
+});
+
+function removeUnneededText(text) {
+    // 
+
+    // Post-DrawSuccessful# DrawnBalanceRes NonRes ResNonResUnrestrictedRestricted
+    let finalText = text;
+    let elimateTextCodes = [
+        'Post-DrawSuccessful',
+        'DrawnBalance',
+        'Res',
+        'NonRes',
+        'DrawnHunt',
+        'Post-Draw Successful',
+        'AdultYouthLandowner (LPP)',
+        'ChoicePreference Points',
+        'UnrestrictedRestricted',
+        'Colorado Parks and Wildlife'
+    ];
+    elimateTextCodes.forEach((eliminateText, i) => {
+        finalText = finalText.replace(eliminateText, '');
+    });
+    return finalText;
+}
+
+function breakUpArrayByDrawCode(elkArr) {
+
+    // scroll through the string/array
+    // 'EE001E1R  ', regex to match the elk unit, from there create a while loop
+    // you will index the first unit and increase count
+    // while 
+    elkArr.forEach(unit => {
+        // if 
+        // while
+    })
+}
+// break up array by draw code
 // crawl pdf
 // figure out code of unit
 // get res and non res info
