a6156348e9b50bc1a1c507bd67ed1bfaf25b9fdb
grantfogle <grantjfogle@gmail.com>
2022-08-29 20:33:02 -0600

    breaking out logic into it's own folder

---
+++ services/parser.js
@@ -0,0 +1,134 @@
+function removeUnneededText(text) {
+    console.log('BING')
+    const elkRegex = /E[A-Z]\d\d\d[A-Z]\d[A-Z]/;
+
+    let i = 0;
+    let huntJson = {};
+    let currentHuntCode = '';
+    
+    while (i < text.length) {
+        const trimmedText = text[i].trim();
+
+        if (elkRegex.test(trimmedText) && trimmedText.length === 8) {
+            currentHuntCode = trimmedText;
+            huntJson[currentHuntCode] = {
+                firstChoice: {
+                    preDraw: [],
+                    postDraw: [],
+                },
+                totalChoice: []
+            }
+        }
+
+        if (trimmedText === 'Pre-Draw Applicants') {
+            const preDrawStats = [];
+            let secondIndex = 1;
+            let pointCount = 0;
+            while (text[i+secondIndex] && !text[i+secondIndex].includes('Post') && !text[i+secondIndex].includes('Total Choice')) {
+                let secondTrimmedText =  text[i+secondIndex].trim();
+
+                if (!isNaN(secondTrimmedText.charAt(0)) && secondTrimmedText.length > 1) {
+                    pointCount++;
+                    preDrawStats.push(secondTrimmedText);
+                }
+                secondIndex++;
+            }
+
+            i += (secondIndex - 1);
+            huntJson[currentHuntCode].firstChoice.preDraw = preDrawStats;
+        }
+
+        if (trimmedText === 'Post-Draw Successful') {
+            let postDrawStats = [];
+            let secondIndex = 1;
+            let pointCount = 0;
+
+            while (text[i+secondIndex] && text[i+secondIndex] !== 'Post-Draw') {
+                let secondTrimmedText =  text[i+secondIndex].trim();
+
+                if (!isNaN(secondTrimmedText.charAt(0)) && secondTrimmedText.length > 1) {
+                    postDrawStats.push(secondTrimmedText);
+                    pointCount++;
+                }
+                secondIndex++;
+            }
+
+            i += (secondIndex - 1);
+            huntJson[currentHuntCode].firstChoice.postDraw = postDrawStats;
+        }
+
+        if (trimmedText.includes('Total Choice')) {
+            huntJson[currentHuntCode].totalChoice.push(trimmedText);
+        }
+
+        i++;
+    }
+
+    organizeHuntCodes(huntJson);
+
+    return huntJson;
+}
+
+function breakdownDrawNumbers(preDraw, postDraw) {
+    const preDrawLen = preDraw.length - 1;
+    let currentPreferencePoint = 0;
+    const huntStr = preDraw;
+
+    for (let i = preDrawLen; i >= 0; i--) {
+        let currentStr = huntStr[i].replace(/\s/g, '');
+        let preferencePoint = 0;
+        let drawStr = '';
+
+        console.log('stinky kaila', currentStr)
+        if (2 < (preDrawLen - i)) {
+            preferencePoint = currentStr.charAt(0);
+            drawStr = currentStr.slice(1, currentStr.length - 1);
+        } else if ((preDrawLen - i) < 10 && currentStr.charAt(0) !== 1) {
+            preferencePoint = currentStr.charAt(0);
+            drawStr = currentStr.slice(1, currentStr.length - 1);
+        } else {
+            preferencePoint = currentStr.slice(0, 2);
+            drawStr = currentStr.slice(2, currentStr.length - 1);
+            // double digit codes 
+        }
+        firstChoiceObj[preferencePoint] = drawStr;
+        // console.log(firstChoiceObj);
+    }
+    // let res = 
+    // structure is [key]
+    // let nonRes = 
+    return {res, nonRes}
+}
+
+function getResNonResNumbers(pointNumbers) {
+    // if length of draw numbers is 6 then we are good
+    // if it's greater then 6 more complex
+    let res = 0;
+    let nonRes = 0;
+    if (pointNumbers.length === 6) {
+        res = pointNumbers.charAt(0);
+        nonRes = pointNumbers.charAt(1);
+    } else {
+        
+    }
+
+}
+
+function organizeHuntCodes(huntObj) {
+    const firstChoice = {};
+    const huntObjFinal = {};
+    // const firstChoiceLength = huntObj.firstChoice.length;
+
+    for (let key in huntObj) {
+        const preDraw = huntObj[key].firstChoice.preDraw;
+        const postDraw =  huntObj[key].firstChoice.postDraw;
+        let firstChoiceObj = {
+            firstChoice: {}
+        }
+
+        firstChoiceObj.firstChoice[key] = breakdownDrawNumbers(preDraw, postDraw);
+    }
+    console.log(huntObjFinal);
+}
+
+module.exports = {removeUnneededText};
